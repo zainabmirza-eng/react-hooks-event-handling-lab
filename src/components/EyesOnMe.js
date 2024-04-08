890da990da1347c2cf6814f065bc37116d9ff9b9
zainab mirza <zainabmirzafatah99@gmail.com>
2024-04-08 16:20:56 +0300

    added

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+        function handleButton(event){
+            console.log('Good!')
+        }
+
+        function handleBlur(event){
+            console.log('Hey! Eyes on me!')
+        };
+    return(
+        <div>
+            <button onFocus={handleButton} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleInput(event){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleInput}/>
+        </div>
     )
 }
 
